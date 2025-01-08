15356a619d614aa5db2950aa4a59d79ca6ec2504
Drum Ogilvie <drummond.ogilvie@novata.com>
2025-01-08 13:26:49 +0000

    Swap vim-surround for nvim-surround

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -70,8 +70,16 @@ return {
         "nvim-tree/nvim-web-devicons",
         opts = { default = true },
     },
-    -- The obligatory tpope entry
-    { "tpope/vim-surround",   event = "BufReadPre" }, -- Nice surround manipulation
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
     -- The obligatory folke section
     -- Nicer diagnostics
     {
